344863323f79231033b1ed94ccc1043b1b1f593e
MritachiUchiha <gamerpubg1008@gmail.com>
2023-08-14 17:32:57 +0200

    Small Update(Beta)
    -Made the "Hi (come here if your installing the bot)" more clean

--- Simple-Bot/Hi (come here if your installing the bot)/installation/Install.bat
+++ Simple-Bot/Hi (come here if your installing the bot)/installation/Install.bat
@@ -2,7 +2,8 @@
 echo "Press Any Key"
 call ./read-me.bat
 echo Download will start 3,2,1,Now!!
-powershell.exe ./install-scoop.ps1
-powershell.exe ./Install-node.js-and-python.ps1
-npm i
+powershell.exe ./Packages/install-scoop.ps1
+powershell.exe ./Packages/Install-node.js-and-python.ps1
+scoop update &nodejs&scoop update python&scoop update&npm i
 PAUSE
+
--- Simple-Bot/Hi (come here if your installing the bot)/installation/read-me.bat
+++ Simple-Bot/Hi (come here if your installing the bot)/installation/read-me.bat
@@ -1,3 +1,4 @@
 @echo off
 echo Go here and download Microsoft Visual(important): visualstudio.microsoft.com/thank-you-downloading-visual-studio/ i suggest you to download the Professional
+echo And don't worry in case you have already installed this node.js and python previously i included to update the node.js and python, in another words just sit and relax until the installation is finished :)
 pause
